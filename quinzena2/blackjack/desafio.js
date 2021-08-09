9f429898ca7049ce2a725d98ce42d7fe573f6066
Caio-Chamarelli <chamarelli08@gmail.com>
2021-08-08 21:19:51 -0300

    Projeto-Blackjack

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,48 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+
+    /* Não consegui parar para fazer os desafios pois voltei de viagem hoje(08/08)
+    vou tentar fazer durante a semana e caso consiga, atualizo o PR*/
+    console.log("Bem vindo(a) ao jogo de Blackjack")
+  
+
+    let blackJack = confirm ("Deseja iniciar nova rodada?")
+ 
+    if (blackJack){
+       let primeiraCartaJogador = comprarCarta()
+       let segundaCartaJogador = comprarCarta()
+       let primeiraCartaIa = comprarCarta()
+       let segundaCartaIa = comprarCarta()
+ 
+ 
+       let pontosJogador = primeiraCartaJogador.valor + segundaCartaJogador.valor
+       let pontosIa = primeiraCartaIa.valor + segundaCartaIa.valor
+    
+ 
+    console.log(`Cartas do Jogador: ${primeiraCartaJogador.texto}, ${segundaCartaJogador.texto} - ${pontosJogador}`)
+    console.log(`Cartas da Ia: ${primeiraCartaIa.texto}, ${segundaCartaIa.texto} - ${pontosIa}`)
+ 
+    if (pontosJogador > pontosIa){
+       console.log("Vencedor: Jogador")
+    }
+    else if (pontosIa > pontosJogador){
+       console.log("Vencedor: Ia")
+    }
+    else if (pontosJogador === pontosIa){
+       console.log("Empate")
+    }
+ 
+ } else {
+    console.log("Fim de jogo")
+ }
