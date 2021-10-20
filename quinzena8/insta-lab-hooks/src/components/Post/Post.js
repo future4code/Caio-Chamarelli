0e644cd198d69f5944ef89ee48a173b686fe5a35
Caio Chamarelli <86692521+Caio-Chamarelli@users.noreply.github.com>
2021-10-20 16:09:11 -0300

    Hooks

--- quinzena8/insta-lab-hooks/src/components/Post/Post.js
+++ quinzena8/insta-lab-hooks/src/components/Post/Post.js
@@ -26,66 +26,66 @@ const Post = (props) => {
       setNumeroCurtidas(numeroCurtidas + 1)
     }
   }
-};
-
-const onClickComentario = () => {
-  setComentando(!comentando)
-};
 
 
-const enviarComentario = (comentario) => {
-  const listaDeComentarios = [...comentarios, comentario]
+  const onClickComentario = () => {
+    setComentando(!comentando)
+  };
 
-  setComentarios(
-    setComentarios(listaDeComentarios),
-    setComentando(false),
+
+  const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+
+
+    setComentarios(listaDeComentarios)
+    setComentando(false)
     setNumeroComentario(numeroComentario + 1)
+
+  }
+
+
+
+  const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+
+  const caixaDeComentario = comentando ? (
+    <SecaoComentario enviarComentario={enviarComentario} />
+  ) : (
+    comentarios.map((comentario) => {
+      return (
+        <CommentContainer>
+          <p>{comentario}</p>
+        </CommentContainer>
+      );
+    })
+  );
+
+
+
+
+  return (
+    <PostContainer>
+      <PostHeader>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'} />
+        <p>{props.nomeUsuario}</p>
+      </PostHeader>
+
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'} />
+
+      <PostFooter>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentario}
+        />
+      </PostFooter>
+      {caixaDeComentario}
+    </PostContainer>
   )
 }
-
-
-
-const iconeCurtida = curtido ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
-
-const caixaDeComentario = comentando ? (
-  <SecaoComentario enviarComentario={enviarComentario} />
-) : (
-  comentarios.map(comentario => {
-    return (
-      <CommentContainer>
-        <p>{comentario}</p>
-      </CommentContainer>
-    )
-  })
-);
-
-
-
-
-return (
-  <PostContainer>
-    <PostHeader>
-      <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'} />
-      <p>{props.nomeUsuario}</p>
-    </PostHeader>
-
-    <PostPhoto src={props.fotoPost} alt={'Imagem do post'} />
-
-    <PostFooter>
-      <IconeComContador
-        icone={iconeCurtida}
-        onClickIcone={onClickCurtida}
-        valorContador={numeroCurtidas}
-      />
-
-      <IconeComContador
-        icone={iconeComentario}
-        onClickIcone={onClickComentario}
-        valorContador={numeroComentario}
-      />
-    </PostFooter>
-    {caixaDeComentario}
-  </PostContainer>
-)
-
 export default Post
